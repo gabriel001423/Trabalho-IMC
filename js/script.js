2fefc04aad67c78d52dcc48befe20e5ebc5c66d1
Gabriel Jesus <166547615+gabriel001423@users.noreply.github.com>
2025-06-17 01:19:11 -0300

    v3

--- js/script.js
+++ js/script.js
@@ -2,28 +2,36 @@ function calcular() {
   let Peso = Number(document.getElementById("Peso").value);
   let Altura = Number(document.getElementById("Altura").value);
   let resultado = document.getElementById("resultado");
-  let foto = document.getElementById("foto")
+  let foto = document.getElementById("foto");
 
   let imc = Peso / (Altura * Altura);
 
   if (imc < 18.6) {
     resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Abaixo do peso)";
-    foto.src = "magro.jpg";
+    foto.src = "img/magro.png";
     foto.style.display = "block";
   } else if (imc >= 18.6 && imc < 25) {
     resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Peso ideal)";
+    foto.src = "img/pesoideal.jpg";
+    foto.style.display = "block";
   } else if (imc >= 25 && imc < 30) {
-    resultado.innerHTML =
-      "Seu IMC é: " + imc.toFixed(2) + " (Levemente acima do peso)";
+    resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Levemente acima do peso)";
+    foto.src = "img/homem.png";
+    foto.style.display = "block";
   } else if (imc >= 30 && imc < 35) {
-    resultado.innerHTML =
-      "Seu IMC é: " + imc.toFixed(2) + " (Obesidade grau 1)";
+    resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Obesidade grau 1)";
+    foto.src = "";
+    foto.style.display = "block";
   } else if (imc >= 35 && imc < 40) {
     resultado.innerHTML =
       "Seu IMC é: " + imc.toFixed(2) + " (Obesidade grau 2)";
+    foto.src = "";
+    foto.style.display = "block";
   } else if (imc >= 40) {
     resultado.innerHTML =
       "Seu IMC é: " + imc.toFixed(2) + " (Obesidade grau 3)";
+    foto.src = "img/obessidade.jpg";
+    foto.style.display = "block";
   } else {
     resultado.innerHTML = "Preencha os campos corretamente.";
   }
